6a62f1888228373cfc6f0ee413e719b53263c577
alwaysmaysix <104639059+alwaysmaysix@users.noreply.github.com>
2023-06-08 19:12:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import os
 from pyrogram import Client
 from os import mkdir
 
-app_id = int(os.environ.get("API_ID", 12345))
-app_key = os.environ.get('API_HASH')
-token = os.environ.get('BOT_TOKEN')
+app_id = '18443762'
+app_key = 'a14eb15b00ffaa1f54a7bf93cd521e92'
+token =  '6214452248:AAFs_yAKjPGeLyI58u_Q_86e1JrA_aiB-hQ'
 
 app = Client("zipBot", app_id, app_key, bot_token=token)
 
